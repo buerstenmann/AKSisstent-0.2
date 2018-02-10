076a1e343078f2c40f1d9dcb0854824d1069f92d
buerstenmann <36341403+buerstenmann@users.noreply.github.com>
2018-02-10 20:33:57 +0100

    Add files via upload

---
+++ app/src/main/java/com/example/ims/aksisstent02/MarkListDataSource.java
@@ -0,0 +1,112 @@
+package ch.rechner.aksistent.notenrechner_2;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Gerry on 05.02.2018.
+ */
+
+public class MarkListDataSource {
+
+    private static final String LOG_TAG = MarkListDataSource.class.getSimpleName();
+
+    private SQLiteDatabase database;
+    private MarkListDbHelper dbHelper;
+
+    private String[] columns = {
+            MarkListDbHelper.COLUMN_ID,
+            MarkListDbHelper.COLUMN_DESC,
+            MarkListDbHelper.COLUMN_SUBJECT,
+            MarkListDbHelper.COLUMN_MARK
+    };
+
+    public MarkListDataSource(Context context) {
+        Log.d(LOG_TAG, "DataSource erzeugt dbHelper.");
+        dbHelper = new MarkListDbHelper(context);
+    }
+
+    public void open() {
+        Log.d(LOG_TAG, "Eine Referenz auf die Datenbank wird jetzt angefragt.");
+        database = dbHelper.getWritableDatabase();
+        Log.d(LOG_TAG, "Datenbank-Referenz erhalten. Pfad zur Datenbank: " + database.getPath());
+    }
+
+    public void close() {
+        dbHelper.close();
+        Log.d(LOG_TAG, "Datenbank mit Hilfe des DbHelpers geschlossen.");
+    }
+
+    public MarkList createMarkList(String subject, String mark, String desc) {
+        ContentValues values = new ContentValues();
+        values.put(MarkListDbHelper.COLUMN_DESC, desc);
+        values.put(MarkListDbHelper.COLUMN_SUBJECT, subject);
+        values.put(MarkListDbHelper.COLUMN_MARK, mark);
+
+        long insertId = database.insert(MarkListDbHelper.TABLE_MARK_LIST, null, values);
+
+        Cursor cursor = database.query(MarkListDbHelper.TABLE_MARK_LIST,
+                columns, MarkListDbHelper.COLUMN_ID + "=" + insertId,
+                null, null, null, null);
+
+        cursor.moveToFirst();
+        MarkList markList = cursorToMarkList(cursor);
+        cursor.close();
+
+        return markList;
+    }
+
+    public void deleteShoppingMemo(MarkList markList) {
+        long id = markList.getId();
+
+        database.delete(MarkListDbHelper.TABLE_MARK_LIST,
+                MarkListDbHelper.COLUMN_ID + "=" + id,
+                null);
+
+        Log.d(LOG_TAG, "Eintrag gelöscht! ID: " + id + " Inhalt: " + markList.toString());
+    }
+
+    private MarkList cursorToMarkList(Cursor cursor) {
+        int idIndex = cursor.getColumnIndex(MarkListDbHelper.COLUMN_ID);
+        int idSubject = cursor.getColumnIndex(MarkListDbHelper.COLUMN_SUBJECT);
+        int idMark = cursor.getColumnIndex(MarkListDbHelper.COLUMN_MARK);
+        int idDesc = cursor.getColumnIndex(MarkListDbHelper.COLUMN_DESC);
+
+        String subject = cursor.getString(idSubject);
+        String mark = cursor.getString(idMark);
+        String desc = cursor.getString(idDesc);
+        long id = cursor.getLong(idIndex);
+
+        MarkList markList = new MarkList(mark, subject, id, desc);
+
+        return markList;
+    }
+
+    public List<MarkList> getAllShoppingMemos() {
+        List<MarkList> markListList = new ArrayList<>();
+
+        Cursor cursor = database.query(MarkListDbHelper.TABLE_MARK_LIST,
+                columns, null, null, null, null, null);
+
+        cursor.moveToFirst();
+        MarkList markList;
+
+        while(!cursor.isAfterLast()) {
+            markList = cursorToMarkList(cursor);
+            markListList.add(markList);
+            Log.d(LOG_TAG, "ID: " + markList.getId() + ", Inhalt: " + markList.toString());
+            cursor.moveToNext();
+        }
+
+        cursor.close();
+
+        return markListList;
+    }
+
+}
---
+++ app/src/main/java/com/example/ims/aksisstent02/MarkListDbHelper.java
@@ -0,0 +1,59 @@
+package ch.rechner.aksistent.notenrechner_2;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by Gerry on 31.01.2018.
+ */
+
+public class MarkListDbHelper extends SQLiteOpenHelper {
+
+    private static final String LOG_TAG = MarkListDbHelper.class.getSimpleName();
+
+    public static final String DB_NAME = "mark_list.db";
+    public static final int DB_VERSION = 1;
+
+    public static final String TABLE_MARK_LIST = "mark_list";
+
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_SUBJECT = "subject";
+    public static final String COLUMN_MARK= "mark";
+    public static final String COLUMN_DESC= "description";
+
+    public static final String SQL_CREATE =
+            "CREATE TABLE " + TABLE_MARK_LIST +
+                    "(" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    COLUMN_SUBJECT + " TEXT NOT NULL, " +
+                    COLUMN_DESC + " TEXT NOT NULL, " +
+                    COLUMN_MARK + " TEXT NOT NULL);";
+
+
+
+    public MarkListDbHelper(Context context) {
+        // super(context, "PLACEHOLDER", null, 1, null);
+        super(context, DB_NAME, null, DB_VERSION);
+        Log.d(LOG_TAG, "DbHelper hat die Datenbank: " + getDatabaseName() + " erzeugt.");
+
+    }
+
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        try {
+            Log.d(LOG_TAG, "Die Tabelle wird mit SQL-Befehl: " + SQL_CREATE + " angelegt.");
+            db.execSQL(SQL_CREATE);
+        }
+        catch (Exception ex) {
+            Log.e(LOG_TAG, "Fehler beim Anlegen der Tabelle: " + ex.getMessage());
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
